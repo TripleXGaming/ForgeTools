941d8b15ee207553b8206c646a32e37e31b5d8cf
StonerSteve <spying247@gmail.com>
2014-11-09 14:56:45 -0500

    Revert "Added spying247 was here"
    This reverts commit 710dfa02ff098c588bdbf335b016804e76d69f8d.

---
+++ main/java/TripleXGaming/ForgeTools/CommonProxy.java
@@ -0,0 +1,22 @@
+package TripleXGaming.ForgeTools;
+
+import net.minecraft.world.World;
+
+public class CommonProxy {
+
+    public void registerRenderInformation() {
+	}
+
+    public World getClientWorld(){
+        return null;
+    }
+    
+	public void registerRenderThings() {
+	}
+	
+	public void registerNetworkStuff(){
+		
+	}
+	
+
+}
