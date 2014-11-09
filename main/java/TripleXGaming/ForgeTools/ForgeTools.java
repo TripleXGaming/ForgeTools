4dc97ba9338844f69f769078b18b72c2b2c1fbbc
TripleXGaming <TripleXGaming@live.com>
2014-11-09 13:20:47 -0600

    Update ForgeTools.java

--- main/java/TripleXGaming/ForgeTools/ForgeTools.java
+++ main/java/TripleXGaming/ForgeTools/ForgeTools.java
@@ -16,6 +16,7 @@ public class ForgeTools
     public static final String MODID = "Forge Tools";
     public static final String VERSION = "0.1";
     //this better be here
+    //test
     @EventHandler
     public void preinit(FMLPreInitializationEvent event)
     {
