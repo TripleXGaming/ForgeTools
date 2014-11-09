03ef30a721157ea7c76aa3dcba6999614f58de43
TripleXGaming <admin@forbiddengaming.net>
2014-11-09 05:05:04 -0600

    Code Updated

---
+++ src/main/java/TripleXGaming/ForgeTools/Commands/CommandHome.java
@@ -0,0 +1,85 @@
+package TripleXGaming.ForgeTools.Commands;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentTranslation;
+
+public class CommandHome implements ICommand
+{
+  private List aliases;
+  public CommandHome()
+  {
+    this.aliases = new ArrayList();
+    this.aliases.add("home");
+  }
+
+  @Override
+  public String getCommandName()
+  {
+    return "home";
+  }
+
+  @Override
+  public String getCommandUsage(ICommandSender icommandsender)
+  {
+    return "home <home name>";
+  }
+
+  @Override
+  public List getCommandAliases()
+  {
+    return this.aliases;
+  }
+
+  @Override
+  public void processCommand(ICommandSender icommandsender, String[] astring){
+    if(astring.length == 0){
+      icommandsender.addChatMessage(new ChatComponentTranslation(ChatFormatting.RED + "/home <home name>"));
+      return;
+    }
+    
+    if(icommandsender instanceof EntityPlayer){
+
+    	EntityPlayer player = (EntityPlayer) icommandsender;
+    	if(player.worldObj.isRemote == false){
+    		
+	    	player.addChatMessage(new ChatComponentTranslation("warping to home: [" + astring[0] + "]"));
+	    	
+	    	EntityPlayerMP thePlayer = (EntityPlayerMP) player;    	
+	    	//List<EntityPlayerMP> thePlayer = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+	    }
+    }
+  }
+  @Override
+  public boolean canCommandSenderUseCommand(ICommandSender icommandsender)
+  {
+    return true;
+  }
+
+  @Override
+  public List addTabCompletionOptions(ICommandSender icommandsender,
+      String[] astring)
+  {
+    return null;
+  }
+
+  @Override
+  public boolean isUsernameIndex(String[] astring, int i)
+  {
+    return false;
+  }
+
+  @Override
+  public int compareTo(Object o)
+  {
+    return 0;
+  }
+  
+}
---
+++ src/main/java/TripleXGaming/ForgeTools/ForgeTools.java
@@ -0,0 +1,44 @@
+package TripleXGaming.ForgeTools;
+
+import TripleXGaming.ForgeTools.Commands.CommandHome;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = ForgeTools.MODID, version = ForgeTools.VERSION , acceptableRemoteVersions = "*")
+public class ForgeTools
+{
+    public static final String MODID = "Forge Tools";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event)
+    {
+        System.out.println(ChatFormatting.RED + MODID + " Version " + VERSION + " is Initializing.");
+    }
+    
+    @EventHandler
+    public void load(FMLInitializationEvent event)
+    {
+    	
+    }
+    
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event)
+    {
+    	
+    }
+    
+	@EventHandler
+	public void serverLoad(FMLServerStartingEvent event){
+		event.registerServerCommand(new CommandHome());
+		
+	}
+}
