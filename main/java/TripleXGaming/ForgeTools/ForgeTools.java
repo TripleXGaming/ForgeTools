1d731cd64b2e528cf1cb2672b92edfdad3d39d40
TripleXGaming <admin@forbiddengaming.net>
2014-11-09 12:46:18 -0600

    remove comment

--- main/java/TripleXGaming/ForgeTools/ForgeTools.java
+++ main/java/TripleXGaming/ForgeTools/ForgeTools.java
@@ -1,5 +1,5 @@
 package TripleXGaming.ForgeTools;
-//test
+
 import TripleXGaming.ForgeTools.Commands.CommandHome;
 import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.init.Blocks;
